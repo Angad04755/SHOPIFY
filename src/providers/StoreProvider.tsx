e0380da05733f1fc80bb940af6f1262dd2a90718
Angad <angad04755@gmail.com>
2026-03-18 09:39:13 +0530

    stage created

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import {store} from "../../store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "../../store/store";
+
 function StoreProvider (props: any) {
     return (
     <Provider store={store}>
