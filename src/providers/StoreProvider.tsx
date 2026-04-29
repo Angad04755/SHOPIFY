284a62adfc1635895491f7c4169894da7ea43f6d
Angad <angad04755@gmail.com>
2026-04-29 18:34:07 +0530

    changes

--- src/providers/StoreProvider.tsx
+++ src/providers/StoreProvider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Provider } from "react-redux";
-import {store} from "../store/store";
+import { store } from "../store/store";
 
 function StoreProvider (props: any) {
     return (
